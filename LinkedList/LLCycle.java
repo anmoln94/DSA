36670207b46debb6e1f858f204df1f114560a214
Shravan Nayak <shravannayak@gmail.com>
2017-08-07 01:59:18 -0700

    Addd LinkedList Cycle

---
+++ LinkedList/LLCycle.java
@@ -0,0 +1,39 @@
+/*
+Create two pointers fast and slow and assign them to head
+
+Move fast = fast.next.next
+Move slow = slow.next
+
+if fast reaches slow then you have a cycle
+
+*/
+
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        if (head == null) {
+            return false;
+        }
+        while (fast.next != null && fast.next.next!=null ) {
+            fast= fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
