74ba81289c72736d0c787f9b4f93a340f7002631
anmoln94 <shramol94@gmail.com>
2017-08-07 20:39:45 +0530

    -

---
+++ LinkedList/MinPathSuminBST.java
@@ -0,0 +1,30 @@
+/*recursively find all paths(sums) from root to leaf. 
+ * Calculate left sum and right sum for each node, and return the smaller
+ * of the two back to the parent.
+ * This way the the root node will be returned the smallest sum from below.
+ * 
+ */
+public static int minPathSum(TreeNode root) {
+        if(root == null) return 0;
+        int sum = root.val;
+        int leftSum = Integer.MAX_VALUE;
+        int rightSum = Integer.MAX_VALUE;
+        if(root.right==null && root.left==null) {
+            return sum;
+        }else{
+
+            if(root.left!=null){
+                leftSum = minPathSum(root.left);
+            }
+            if (root.right!=null){
+                rightSum = minPathSum(root.right);
+            }
+            if(leftSum < rightSum){
+                sum += leftSum;
+            }else{
+                sum += rightSum;
+            }
+        }
+
+        return sum;
+    }
