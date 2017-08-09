d87695a0f93ee2db81409d8fcc33da810a14175e
Anmol Nayak <shramol94@gmail.com>
2017-08-09 18:25:47 +0530

    -

--- Arrays/ElementsoccuringOddtimesandEventimes.java
+++ Arrays/ElementsoccuringOddtimesandEventimes.java
@@ -1,3 +1,13 @@
+/* Elements occuring odd number of times: Create Hashset. Remove the element if it already exists in set. 
+At the end, all remaining elements occur odd times.
+
+Elements occuring even number of times: Create Hashmap. If element does not exist, add it to the hashmap with value 0^arr[i].
+If it already exists change its value to arr[i]^arr[i].
+(Since exor of even number of times occuring elements is 0).
+At the end, all the keys which have value 0, occur even number of times.
+*
+*/
+
 package Arrays;
 
 import java.util.HashMap;
@@ -18,8 +28,8 @@ public class ElementsoccuringOddtimes {
 		
 		for(int i=0;i<arr.length;i++)
 		{
-			map.put(arr[i], map.getOrDefault(arr[i],0)^arr[i]);
-			if(set.contains(arr[i]))
+			map.put(arr[i], map.getOrDefault(arr[i],0)^arr[i]);	//For detecting even number of times
+			if(set.contains(arr[i]))				//For detecting odd number of times
 				set.remove(arr[i]);
 			else
 				set.add(arr[i]);
