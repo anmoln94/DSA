11288a4ea0b249d6243d31e2241bc26d1b47da74
Anmol Nayak <shramol94@gmail.com>
2017-08-10 14:49:20 +0530

    -

---
+++ Trees/BinaryTreePaths.java
@@ -0,0 +1,33 @@
+/*Return a list of all the paths. At each recursive call, at the root.val to the path string. 
+If both left and child are null, add the current path to the list.
+*/
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution 
+{
+    public List<String> binaryTreePaths(TreeNode root) 
+    {
+        List<String> answer = new ArrayList<String>();
+        if (root != null) 
+            searchBT(root, "", answer);
+        return answer;
+    }
+    private void searchBT(TreeNode root, String path, List<String> answer) 
+    {
+        if (root.left == null && root.right == null) 
+            answer.add(path + root.val);
+        if (root.left != null) 
+            searchBT(root.left, path + root.val + "->", answer);
+        if (root.right != null) 
+            searchBT(root.right, path + root.val + "->", answer);
+    }
+}
