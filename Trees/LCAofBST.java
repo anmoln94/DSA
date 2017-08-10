3d87a11880e20b037c38662bf7208a2962fe8f8d
Anmol Nayak <shramol94@gmail.com>
2017-08-10 14:58:08 +0530

    -

--- Trees/LCAofBST.java
+++ Trees/LCAofBST.java
@@ -1,10 +1,9 @@
 /*
--  p and q are in the same subtree (meaning their values are both smaller or both larger than root's).
-
 Recursively check:
 - if P.val and q.val both greater than root then return lowestCommonAncestor(root.left, p, q)
 - if P.val and q.val both lesser than root then return lowestCommonAncestor(root.right, p, q)
-
+else
+    return root (since P and Q will be in different sub trees )
 */
 
 /**
@@ -22,5 +21,7 @@ public class Solution {
             return lowestCommonAncestor(root.left, p, q);
         else if(root.val < p.val && root.val < q.val) 
             return lowestCommonAncestor(root.right, p, q);
+        else
+            return root;
     }
 }
